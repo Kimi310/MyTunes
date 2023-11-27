633cb76385422a2e60e83ccbc5ad136efd560d9c
Kimi310 <94898434+Kimi310@users.noreply.github.com>
2023-11-27 13:33:00 +0100

    View for adding a song

---
+++ src/GUI/Controller/AddSongView.java
@@ -0,0 +1,15 @@
+package GUI.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddSongView {
+    @FXML
+    private TextField titletxt;
+    @FXML
+    private TextField artisttxt;
+    @FXML
+    private TextField timetxt;
+    @FXML
+    private TextField filetxt;
+}
